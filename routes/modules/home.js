20ebacb505e7bc626258b48a34b0a0cfd6e088fb
Ann Huang <s52561@yahoo.com.tw>
2020-09-17 16:20:07 +0800

    feat:add totalAmount

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,37 +4,60 @@ const router = express.Router()
 // 引用 record model
 const Record = require('../../models/record')
 const Category = require('../../models/category')
-// 定義首頁路由
+
 
 // router.get('/', (req, res) => {
+//   let category = []
 //   Record.find()
+//     .sort({ _id: 1 })
 //     .lean()
-//     .then(record => res.render('index', { record }))
-//     .catch(error => console.error(error))
-//   // past the movie data into 'index' partial template
-//   // res.render('index', { restaurants: results })
+//     .then(
+//       record => {
+//         record.forEach(item => {
+//           let cato = []
+//           Category.find({ name: item.category })
+//             .lean()
+//             .then(cate => {
+//               cato.push(cate[0].tag)
+//               category.push(cato)
+//             })
+//             .catch(error => console.error(error))
+//         })
+//         res.render('index', { record, category })
+//       })
+//     .catch(err => console.error(err))
 // })
+// // 匯出路由模組
+// module.exports = router
 
+
+
+//all view
 router.get('/', (req, res) => {
-  let category = []
   Record.find()
-    .sort({ _id: 1 })
     .lean()
-    .then(
-      record => {
-        record.forEach(item => {
-          let cato = []
-          Category.find({ name: item.category })
-            .lean()
-            .then(cate => {
-              cato.push(cate[0].tag)
-              category.push(cato)
-            })
-            .catch(error => console.error(error))
-        })
-        res.render('index', { record, category })
-      })
-    .catch(err => console.error(err))
+    .sort({ date: 'desc' })
+    .then(records => {
+      let totalAmount = 0
+      if (records.length !== 0) {
+        totalAmount = records.map(record => Number(record.amount)).reduce((total, amount) => total + amount)
+      }
+      res.render('index', { totalAmount, records })
+    })
 })
-// 匯出路由模組
+
+router.get('/filter/:category', (req, res) => {
+  Record.find({ category: `${req.params.category}` })
+    .lean()
+    .sort({ date: 'desc' })
+    .then(records => {
+      let totalAmount = 0
+      if (records.length !== 0) {
+        totalAmount = records.map(record => Number(record.amount)).reduce((total, amount) => total + amount)
+      }
+      const params = req.params.category
+      res.render('index', { records, totalAmount, params })
+    })
+})
+
 module.exports = router
