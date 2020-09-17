f4f062b1c1cc77d864a3dab437e526392eee5720
Ann Huang <s52561@yahoo.com.tw>
2020-09-17 20:50:50 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,21 @@
 const express = require('express')
 const router = express.Router()
 // 準備引入路由模組
-
 // 引入 home 模組程式碼
 const home = require('./modules/home')
+// 引入 res 模組程式碼
+const expense = require('./modules/expense')
+const users = require('./modules/users')  // add this
+
+
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 
-// 引入 res 模組程式碼
-const expense = require('./modules/expense')
-// 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
-router.use('/expense', expense)
 
+
+// 將網址結構符合 /expense 字串開頭的 request 導向 expense 模組
+router.use('/expense', expense)
+// 總路由器引用 users 模組
+router.use('/users', users)
 // 匯出路由器
 module.exports = router
